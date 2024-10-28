69b0b3eb8406d1eb6ad86bbe2eca6b1d2da4d430
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2024-10-28 10:58:17 +0900

    asdf

---
+++ examples/primitive_values.rs
@@ -0,0 +1,34 @@
+use polars::df;
+use polars_deser_row::{deserialize_all, deserialize_single_row};
+
+fn main() {
+    #[derive(serde::Deserialize, Debug, PartialEq, Eq)]
+    struct PrimitiveTyInt {
+        bool: bool,
+        int8: i8,
+        int16: i16,
+        int32: i32,
+        int64: i64,
+        uint8: u8,
+        uint16: u16,
+        uint32: u32,
+        uint64: u64,
+    }
+
+    let df = df!(
+        "bool" => [false, true, false],
+        "int8" => [1i8, 2, 3],
+        "int16" => [1i16, 2, 3],
+        "int32" => [1i32, 2, 3],
+        "int64" => [1i64, 2, 3],
+        "uint8" => [1u8, 2, 3],
+        "uint16" => [1u16, 2, 3],
+        "uint32" => [1u32, 2, 3],
+        "uint64" => [1u64, 2, 3],
+    )
+    .unwrap();
+
+    let ty: PrimitiveTyInt = crate::deserialize_all(df.clone()).unwrap();
+    println!("{ty:?}");
+    println!("{df}");
+}
